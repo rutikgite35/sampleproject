e4cc53265fd54132f6014716850284e54e10fa33
sai <sai@DESKTOP-PLL9V1D>
2023-06-09 12:10:54 +0530

    Add project files.

---
+++ sampleproject/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter a number : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.WriteLine($"{num} is even");
+}
+else
+{
+    Console.WriteLine($"{num} is odd");
+}
